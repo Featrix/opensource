c26911a6ed2425c6e1aa3ea124b5e43d654bbb76
Scott Jones <scott@roli.ai>
2024-09-12 11:18:52 -0400

    Refactored csharp a bit, made it a bit easier to use for .net devs.

---
+++ csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FeatrixExample;
+
+var clientId = "<fill this in>";
+var clientSecret = "<fill this in>";
+var url = "<fill this in>";
+var neuralFunctionId = "<fill this in>";
+
+try {
+    // Create instance of Featrix
+    var featrix = await Featrix.CreateAsync(clientId, clientSecret, url, true, true);
+    
+    // Run prediction
+    var prediction = await featrix.PredictAsync(neuralFunctionId, new List<Dictionary<string, object>>());
+    
+    var json = JsonSerializer.Serialize(prediction);
+    Console.WriteLine("Prediction returned: " + json);
+}
+catch(Exception ex) {
+    Console.Error.WriteLine("Unhandled exception: " + ex.ToString());
+}
